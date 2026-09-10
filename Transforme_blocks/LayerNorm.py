f1227c251b8f16195a3106d96eee3e64149582ec
John <fetcher_1001@outlook.com>
2026-09-10 18:10:31 +0200

    fix: correct typo in LayerNorm class variable name

--- Transforme_blocks/LayerNorm.py
+++ Transforme_blocks/LayerNorm.py
@@ -4,12 +4,12 @@ import torch.nn as nn
 class LayerNorm(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
-        self.esp = 1e-5
+        self.eps = 1e-5
         self.scale = nn.Parameter(torch.ones(emb_dim))
         self.shift = nn.Parameter(torch.zeros(emb_dim))
 
     def forward(self, x):
         mean = x.mean(dim=-1, keepdim=True)
         var = x.var(dim=-1, keepdim=True, unbiased=False)
-        x_norm = (x - mean) / torch.sqrt(var + self.esp)
+        x_norm = (x - mean) / torch.sqrt(var + self.eps)
         return self.scale * x_norm + self.shift
